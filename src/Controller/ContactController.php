4ba54ea6a82409831146f39112fa93a78c68d098
PMWeill <pierre-manuel.weill@laposte.net>
2023-10-10 16:31:13 +0200

    ajout contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use App\Entity\Contact;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, ManagerRegistry $formul): Response
     {
+
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $formul->getManager();
+            $manager->persist($contact);
+            $manager->flush();
+            return $this->redirectToRoute("app_contact");
+        }
+
+
+
+
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            "formulaire" => $form->createView()
         ]);
     }
+
+
+
+
+
 }
